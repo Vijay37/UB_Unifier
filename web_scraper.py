6e72e37d586c9e403b79557e1ddde5fcbe15e275
Vijay37 <reachpallaharsha@gmail.com>
2018-09-25 21:07:05 -0400

    Able to extract relevant information

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,13 @@
 # Librarires Import
-import urllib
+import urllib2
 from bs4 import BeautifulSoup
 from selenium import webdriver
 url = "https://calendar.buffalo.edu/";  # URL Page from where we are scraping
-browser = webdriver.PhantomJS();
+browser = webdriver.PhantomJS(); # webdriver to load the data properly
 browser.get(url);
 html = browser.page_source;
-# parse the html using beautiful soup and store in variable 'soup'
-soup = BeautifulSoup(html, 'html.parser');
-print (soup);
-#articles = soup.find('div',attrs={'section':'list-event-preview'});
-#print (articles);
+# print(html);
+# # parse the html using beautiful soup and store in variable 'soup'
+soup = BeautifulSoup(html, 'lxml');
+articles = soup.findAll('section',{'class':'list-event-preview'});
+print(articles[0].find('p'));
