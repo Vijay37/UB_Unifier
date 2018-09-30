aa0d0cfc60018207f235b2d73c48a087ba05830d
Vijay37 <reachpallaharsha@gmail.com>
2018-09-29 22:04:38 -0400

    Wrote a code to extract club info, event info and date and append it to CSV file. Need to clean the date text

--- web_scraper-link2.py
+++ web_scraper-link2.py
@@ -10,4 +10,20 @@ html = browser.page_source;
 #
 soup = BeautifulSoup(html, 'lxml');
 divs = soup.findAll('div',{'id':'event-discovery-list'});
-parent_div = divs[0]
+parent_div = divs[0];
+divs = parent_div.findAll('a')
+count=0
+events=[]
+dates=[]
+clubs=[]
+for div in divs:
+    event = div.find('h3').get_text()
+    events.append(event)
+    club = div.findAll('span')[3].get_text()
+    clubs.append(club)
+    time = div.get_text().replace(event,"")
+    time = time.replace(club,"")
+    dates.append(time)
+wtr = csv.writer(open ('out_clubs.csv', 'a'), delimiter=',', lineterminator='\n')
+for f, b,c in zip(clubs, events,dates):
+    wtr.writerow ([f,b,c])
