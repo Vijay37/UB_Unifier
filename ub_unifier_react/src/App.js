b0a24aa32e8facae1eef6ed9f535f1bd07ddd88d
Vijay37 <reachpallaharsha@gmail.com>
2018-10-03 22:04:07 -0400

    Implemented simple login.js

--- ub_unifier_react/src/App.js
+++ ub_unifier_react/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Login from "./containers/Login";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">UB Unifier</h1>
         </header>
-        <p className="App-intro">
-          UB Unifier.
-        </p>
+        <Login />
       </div>
     );
   }
