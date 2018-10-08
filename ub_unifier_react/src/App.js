fc7871db4be820137848c7b6657eccbb8af3a55f
Vijay37 <reachpallaharsha@gmail.com>
2018-10-07 20:05:24 -0400

    Implemented simple backend PHP api and tested fetch functionality

--- ub_unifier_react/src/App.js
+++ ub_unifier_react/src/App.js
@@ -2,7 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./containers/Login";
+const API="http://localhost:8888/Unifier_API/API.php";
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.load_php=this.load_php.bind(this);
+    this.state={
+      key:""
+    }
+  }
+  load_php(){
+    var formData = new FormData();
+    formData.append('KEY',"test");
+    return fetch(API,{
+      method:"POST",
+      mode: "cors",
+      cache:"no-cache",
+      headers:{
+        // 'Accept': 'application/json',
+        // 'Content-Type': 'application/json'
+      },
+      body:formData,
+    }
+  )
+  .then(response=>response.json())
+  .then(function(data){
+    console.log("Data:",data);
+  })
+  .catch(function(error){
+    console.log("Request failed",error);
+  })
+  ;
+  }
   render() {
     return (
       <div className="App">
@@ -10,6 +41,9 @@ class App extends Component {
           <h1 className="App-title">UB Unifier</h1>
         </header>
         <Login />
+        <button onClick={this.load_php}>
+          TEST
+        </button>
       </div>
     );
   }
