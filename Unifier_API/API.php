e2cf3ec925c55870a84f61602d0555b5e84c2171
Vijay37 <reachpallaharsha@gmail.com>
2018-10-31 17:26:08 -0400

    Implemented API to add user event to the database

--- Unifier_API/API.php
+++ Unifier_API/API.php
@@ -5,6 +5,7 @@ header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 header ("Access-Control-Allow-Headers: *");
 include_once("sql_connection.php");
 include_once("UserSession.php");
+include_once("eventHandler.php");
 if(isset($_POST["KEY"])){
   if($_POST["KEY"]=="LOGIN"){
     $response=validateUser($_POST["EMAIL"],$_POST["PASSWORD"]);
@@ -18,6 +19,13 @@ if(isset($_POST["KEY"])){
     $response=logout_user();
     echo json_encode($response);
   }
-
+  else if($_POST["KEY"]=="ADDLINK"){
+    $response=add_user_favlink($_POST["EMAIL"],$_POST["LINK"]);
+    echo json_encode($response);
+  }
+  else if($_POST["KEY"]=="ADDUSEREVENT"){
+    $response=add_user_event($_POST["EMAIL"],$_POST["EVENTNAME"],$_POST["DESC"],$_POST["TIME"],$_POST["DATE"],$_POST["LOCATION"],$_POST["CATEGORY"]);
+    echo json_encode($response);
+  }
 }
 ?>
--- Unifier_API/eventHandler.php
+++ Unifier_API/eventHandler.php
@@ -21,4 +21,25 @@
       }
       return $ret;
     }
+    function add_user_event($emailId,$event_name,$event_desc="",$time,$date,$location="",$category=""){
+      $ret['message'] = "SUCCESS";
+      $ret['STATUS'] = "SUCCESS";
+      $sql_conn = mysqli_connection();
+      if(!($stmt = $sql_conn->prepare("INSERT INTO UserEvents(userEmail,event_name,description,`time`,`date`,location,category) VALUES (?,?,?,?,?,?,?)"))){
+        $ret["message"] =  "Statement preparation failed: (" . $sql_conn->errno . ") " . $sql_conn->error;
+        $ret['STATUS'] = "FAILURE";
+        return $ret;
+      }
+      if(!($stmt->bind_param("sssssss",$emailId, $event_name,$event_desc,$time,$date,$location,$category))){
+        $ret["message"] ="Binding Parameters Failed.";
+        $ret['STATUS'] = "FAILURE";
+        return $ret;
+      }
+      if (!($res = $stmt->execute())) {
+        $ret["message"] =  "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        $ret['STATUS'] = "FAILURE";
+        return $ret;
+      }
+      return $ret;
+    }
 ?>
