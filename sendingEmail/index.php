e822e5ffd9ce6646d51e119afda00171a9d3a7bc
kchikte <kchikte@buffalo.edu>
2018-11-09 12:41:36 -0500

    Added function to generate random token to the verification link

--- sendingEmail/index.php
+++ sendingEmail/index.php
@@ -1,29 +1,32 @@
 <?php
-	require_once "Mail.php";
-	$from = "UB Unifier <ubunifier.5683@gmail.com>";
-	$to = "Vijay Harshavardhan Palla <vijayaha@buffalo.edu>";
-	$cc = "Kalyani Chikte <kchikte@buffalo.edu>";
+	$to = "vijayaha@buffalo.edu";
 	$subject = "UB Unifier verification link";
+	
 	$link = "link";
-	$body = "Hi,\nPlease click the following to verify your account for UB Unifier : ".$link;
-	$host = "ssl://smtp.gmail.com";
-	$port = "465";
-	$username = "ubunifier.5683@gmail.com";
-	$password = "Kalyani@1";
-	$headers = array ('From' => $from,
-	  'To' => $to,
-	  'Cc' => $cc,
-	  'Subject' => $subject);
-	$smtp = Mail::factory('smtp',
-	  array ('host' => $host,
-	  	'port' => $port,
-	    'auth' => true,
-	    'username' => $username,
-	    'password' => $password));
-	$mail = $smtp->send($to, $headers, $body);
-	if (PEAR::isError($mail)) {
-	  echo("<p>" . $mail->getMessage() . "</p>");
-	 } else {
-	  echo("<p>Message successfully sent!</p>");
+	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$digits = '0123456789';
+	$token = generateRandomString($chars);
+	$token .= generateRandomString($digits);
+	$link = "link".$token;
+
+	$txt = "Hi,\nPlease click the following to verify your account for UB Unifier : ".$link;
+	$headers = "From: ubunifier.5683@gmail.com" . "\r\n" .
+	"CC: kchikte@buffalo.edu";
+
+	$retVal = mail($to,$subject,$txt,$headers);
+	if (retVal) {
+		echo("<p>Message successfully sent!</p>");
+	} else {
+		echo("<p>Error while sending email!</p>");
+	}
+
+	function generateRandomString($characters) {
+		$length = 5;
+	    $charactersLength = strlen($characters);
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    return $randomString;
 	}
 ?>
